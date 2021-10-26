55d64d433b8b99f8c5b5465f5437aaa2b506096e
MarlonMoreno <marlonmoreno894@gmail.com>
2021-10-26 18:18:20 -0500

    Create and implement Genre component

---
+++ src/components/Genre.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+
+class Genre extends Component {
+
+    render() {
+        return(
+            <div>
+                <p1>{this.props.name}</p1>
+            </div>
+        )
+    }
+}
+
+export default Genre
